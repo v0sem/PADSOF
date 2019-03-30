a70a6af9e768d8f62b99158b742303f338fba9ed
v0sem <progpabsanch@gmail.com>
2019-03-30 18:18:02 +0100

    Commented Comment, Notification and Status

--- src/padsof/Status.java
+++ src/padsof/Status.java
@@ -1,5 +1,11 @@
 package padsof;
 
+/**
+ * Marca si un metodo se ha ejecutado correctamente
+ * 
+ * @author Equipo 1
+ *
+ */
 public enum Status {
 	OK, ERROR
 }
--- src/padsof/interactions/Comment.java
+++ src/padsof/interactions/Comment.java
@@ -1,13 +1,30 @@
 package padsof.interactions;
 
 import padsof.user.User;
-
+/**
+ * Comentarios realizados por los ususarios
+ * 
+ * @author Equipo 1
+ *
+ */
 public class Comment {
 
+	/**
+	 * Texto contenido del comentario
+	 */
 	private String commentText;
 	
+	/**
+	 * Autor del comentario
+	 */
 	private User commentAuthor;
 	
+	/**
+	 * Constructor de clase comentario
+	 * 
+	 * @param txt Contenido del comentario
+	 * @param auth Autor
+	 */
 	public Comment(String txt, User auth) {
 		
 		this.commentText = txt;
@@ -16,11 +33,21 @@ public class Comment {
 	
 	/*************** GETTERS ********************/
 	
+	/**
+	 * Getter del contenido
+	 * 		
+	 * @return contenido del comentario
+	 */
 	public String getText() {
 		
 		return this.commentText;
 	}
 	
+	/**
+	 * Getter del autor
+	 * 
+	 * @return autor del comentario
+	 */
 	public User getAuthor() {
 		
 		return this.commentAuthor;
