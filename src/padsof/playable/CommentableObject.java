387dbc072f884f182e89c8d0082e142db9f8de86
v0sem <progpabsanch@gmail.com>
2019-03-30 12:16:03 +0100

    Add Comment and Remove Comment functions

--- src/padsof/playable/CommentableObject.java
+++ src/padsof/playable/CommentableObject.java
@@ -2,7 +2,10 @@ package padsof.playable;
 
 import java.util.ArrayList;
 
+import padsof.Status;
 import padsof.interactions.Comment;
+import padsof.user.User;
+import padsof.user.UserType;
 
 public abstract class CommentableObject extends PlayableObject{
 	
@@ -15,5 +18,27 @@ public abstract class CommentableObject extends PlayableObject{
 	// Obtiene la lista de comentarios de un objeto comentable
 	public ArrayList<Comment> getCommentList() {
 		return commentList;
+	}
+	
+	public Status addComment(User auth, String text) {
+		if(text.length() > 0) {
+			this.commentList.add(new Comment(text, auth));
+			return Status.OK;
+		}
+		
+		return Status.ERROR;
+	}
+	
+	public Status removeComment(User auth, String text) {
+		
+		for(Comment com : this.commentList) {
+			if(text.contentEquals(com.getText()) && 
+					(com.getAuthor() == auth || auth.getUserType() == UserType.ADMIN)){
+				this.commentList.remove(com);
+				return Status.OK;
+			}
+		}
+		
+		return Status.ERROR;
 	}
 }
