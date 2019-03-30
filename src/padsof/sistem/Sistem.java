78fb13d9b66e0fde9b65300fa6b26d7b97878450
v0sem <progpabsanch@gmail.com>
2019-03-30 22:02:01 +0100

    Commented Sistem

--- src/padsof/sistem/Sistem.java
+++ src/padsof/sistem/Sistem.java
@@ -15,36 +15,90 @@ import padsof.playable.Song;
 import padsof.user.User;
 import padsof.user.UserType;
 
+/**
+ * Clase principal de la aplicacion
+ * 
+ * @author David Palomo, Pablo Sanchez, Antonio Solana
+ *
+ */
 public class Sistem implements java.io.Serializable {
 	
+	/**
+	 * Instancia del sistema
+	 */
 	private static Sistem instance = null;
 	
+	/**
+	 * Usuario administrador
+	 */
 	private User adminUser = null;
 	
+	/**
+	 * Usuario loggeado actualmente
+	 */
 	private User loggedUser = null; //mientras este a null, estamos en anon
 	
+	/**
+	 * Numero de canciones que a los usuarios anonimos les queda por reproducir
+	 */
 	private long anonSongCount;
 	
+	/**
+	 * Lista de usuarios
+	 */
 	private ArrayList<User> userList;
 	
+	/**
+	 * Lista de canciones
+	 */
 	private ArrayList<Song> songList;
 	
+	/**
+	 * Lista de albumes 
+	 */
 	private ArrayList<Album> albumList;
 	
+	/**
+	 * Lista de Playlists
+	 */
 	private ArrayList<Playlist> playlistList;
 	
+	/**
+	 * Lista de reportes
+	 */
 	private ArrayList<Report> reportList;
 	
+	/**
+	 * Fecha en la que se resetean el numero de canciones reproducibles por los usuarios
+	 * anonimos
+	 */
 	private LocalDate songCountDate;
 	
+	/**
+	 * Precio configurable para ser premium
+	 */
 	private double premiumPrice;
 	
+	/**
+	 * Maximo configurable del numero de canciones que un usuario anonimo puede reproducir
+	 */
 	private long maxAnonSong;
 	
+	/**
+	 * Maximo configurable del numero de canciones que un usuario registrado puede reproducir
+	 */
 	private long maxRegisteredSong;
 	
+	/**
+	 * Numero de veces que tienen que ser reproducidas las canciones de un usuario para 
+	 * que se le pase automaticamente a premium
+	 */
 	private long playsToPremium;
 	
+	/**
+	 * Constructor de la clase Sistem. Inicializa con valores por defecto y con
+	 * un usuario Admin User. Carga los datos de un fichero si este existe
+	 */
 	public Sistem() {
 	
 		//si existe un archivo para cargarlo, lo carga
@@ -74,6 +128,11 @@ public class Sistem implements java.io.Serializable {
 		}
 	}
 	
+	/**
+	 * Permite recoger la instancia de sistema y que solo haya un sistema
+	 * 
+	 * @return el Sistem
+	 */
 	public static Sistem getInstance() {
 		
 		if(instance == null)
@@ -82,6 +141,11 @@ public class Sistem implements java.io.Serializable {
 		return instance;
 	}
 
+	/**
+	 * Carga los datos de Sistem de un fichero System.bal
+	 * 
+	 * @return Sistem cargado
+	 */
 	public Sistem loadData(){
 		try {
 			FileInputStream fin = new FileInputStream("System.bal");
@@ -95,6 +159,7 @@ public class Sistem implements java.io.Serializable {
 			this.playlistList = loadedSystem.playlistList;
 			this.userList = loadedSystem.userList;
 			
+			loadedSystem.checkDate();
 			ois.close();
 			return loadedSystem;
 		}
@@ -103,6 +168,9 @@ public class Sistem implements java.io.Serializable {
 		}
 	}
 	
+	/**
+	 * Comprueba la fecha de hoy y realiza todas las acciones necesarias dependiendo del resultado
+	 */
 	public void checkDate() {
 		LocalDate today = LocalDate.now();
 		Period period;
@@ -161,64 +229,127 @@ public class Sistem implements java.io.Serializable {
 	
 	/************************** Getters ***************************/
 	
+	/**
+	 * Getter de AdminUser
+	 * 
+	 * @return admin user
+	 */
 	public User getAdminUser() {
 	
 		return adminUser;
 	}
 	
+	/**
+	 * Getter del usuario loggeado
+	 * 
+	 * @return usuario loggeado o null si no hay nadie loggeado
+	 */
 	public User getLoggedUser() {
 		
 		return loggedUser;
 	}
 
+	/**
+	 * Getter de AnonSongCount
+	 * 
+	 * @return numero de reproducciones que le quedan a los usuarios anonimos
+	 */
 	public Long getAnonSongCount() {
 		
 		return anonSongCount;
 	}
 	
+	/**
+	 * Getter de PremiumPrice
+	 * 
+	 * @return precio para ser premium
+	 */
 	public double getPremiumPrice() {
 		return premiumPrice;
 	}
 	
+	/**
+	 * Getter de MaxAnonSongCount
+	 * 
+	 * @return numero maximo de reproducciones de los anonimos
+	 */
 	public long getMaxAnonSong() {
 		return maxAnonSong;
 	}
 
+	/**
+	 * Getter de MaxRegisteredSong
+	 * 
+	 * @return numero maximo de reproducciones de los registrados
+	 */
 	public long getMaxRegisteredSong() {
 		return maxRegisteredSong;
 	}
 	
+	/**
+	 * Getter de MaxRegisteredSong
+	 * 
+	 * @return numero maximo de reproducciones de los registrados
+	 */
 	public long getPlaysToPremium() {
 		return playsToPremium;
 	}
 	
 	/*************************** Setters ***************************/
 	
+	/**
+	 * Decreases by one anonSongCount
+	 */
 	public void increaseAnonSongCount() {
 		
 		anonSongCount--;
 	}
 	
+	/**
+	 * Setter de MaxAnonSong
+	 * 
+	 * @param count nuevo maximo
+	 */
 	public void setMaxAnonSong(long count) {
 		
 		maxAnonSong = count;
 	}
 
+	/**
+	 * Setter de PremiumPrice
+	 * 
+	 * @param premiumPrice nuevo precio
+	 */
 	public void setPremiumPrice(double premiumPrice) {
 		this.premiumPrice = premiumPrice;
 	}
 
+	/**
+	 * Setter de MaxRegisteredSong
+	 * 
+	 * @param maxRegisteredSong nuevo maximo
+	 */
 	public void setMaxRegisteredSong(long maxRegisteredSong) {
 		this.maxRegisteredSong = maxRegisteredSong;
 	}
 
-
+	/**
+	 * Setter de PlaysToPremium
+	 * 
+	 * @param playsToPremium nueva meta
+	 */
 	public void setPlaysToPremium(long playsToPremium) {
 		this.playsToPremium = playsToPremium;
 	}
 
 	/********************** Adding to Lists ************************/
 	
+	/**
+	 * Aniade un user al sistema
+	 * 
+	 * @param user un usuario
+	 * @return ERROR en caso de que el user sea null
+	 */
 	public Status addUser(User user) {
 		if(user != null) {
 			this.userList.add(user);
@@ -228,6 +359,12 @@ public class Sistem implements java.io.Serializable {
 		return Status.ERROR;
 	}
 	
+	/**
+	 * Aniade una cancion al sistema
+	 * 
+	 * @param s una cancion
+	 * @return ERROR en caso de que la cancion sea null
+	 */
 	public Status addSong(Song s) {
 		
 		if(s != null) {
@@ -238,6 +375,12 @@ public class Sistem implements java.io.Serializable {
 		return Status.ERROR;
 	}
 	
+	/**
+	 * Aniade un album al sistema
+	 * 
+	 * @param a un album
+	 * @return ERROR en caso de que el album sea null
+	 */
 	public Status addAlbum(Album a) {
 		
 		if(a != null) {
@@ -248,6 +391,12 @@ public class Sistem implements java.io.Serializable {
 		return Status.ERROR;
 	}
 	
+	/**
+	 * Aniade un al sistema
+	 * 
+	 * @param pl una playlist
+	 * @return ERROR en caso de que la playlist sea null
+	 */
 	public Status addPlaylist(Playlist pl) {
 		
 		if(pl != null) {
@@ -260,6 +409,12 @@ public class Sistem implements java.io.Serializable {
 	
 	/******************* Removing from lists *****************/
 	
+	/**
+	 * Elimina un user del sistema
+	 * 
+	 * @param user a eliminar
+	 * @return ERROR en el caso de que user sea null
+	 */
 	public Status deleteUser(User user) {
 		
 		if(user != null) {
@@ -270,6 +425,12 @@ public class Sistem implements java.io.Serializable {
 		return Status.ERROR;
 	}
 
+	/**
+	 * Elimina una cancion del sistema
+	 * 
+	 * @param s a eliminar
+	 * @return ERROR en el caso de que s sea null
+	 */
 	public Status deleteSong(Song s) {
 		
 		if(s != null) {
@@ -280,6 +441,12 @@ public class Sistem implements java.io.Serializable {
 		return Status.ERROR;
 	}
 	
+	/**
+	 * Elimina un album del sistema
+	 * 
+	 * @param a a eliminar
+	 * @return ERROR en el caso de que a sea null
+	 */
 	public Status deleteAlbum(Album a) {
 		
 		if(a != null) {
@@ -290,6 +457,12 @@ public class Sistem implements java.io.Serializable {
 		return Status.ERROR;
 	}
 
+	/**
+	 * Elimina una playlist del sistema
+	 * 
+	 * @param pl a eliminar
+	 * @return ERROR en el caso de que pl sea null
+	 */
 	public Status deletePlaylist(Playlist pl) {
 		
 		if(pl != null) {
@@ -302,6 +475,13 @@ public class Sistem implements java.io.Serializable {
 	
 	/******************** Getting from lists *******************/
 	
+	/**
+	 * Comprueba si ya hay alguien que tiene este nick
+	 * 
+	 * @param userNick a comprobar
+	 * 
+	 * @return true si ya existe
+	 */
 	private Boolean userNickExists(String userNick) {
 		
 		for(User user : this.userList) {
@@ -313,6 +493,12 @@ public class Sistem implements java.io.Serializable {
 		return false;
 	}
 	
+	/**
+	 * Comprueba si ya hay alguien que tiene este nombre
+	 * 
+	 * @param userName a comprobar
+	 * @return true si ya existe
+	 */
 	private Boolean userNameExists(String userName) {
 		if (this.userList == null)
 			System.out.println("user list esta a nul"); // #DEBUG
@@ -327,6 +513,16 @@ public class Sistem implements java.io.Serializable {
 	
 	/************************ User related *********************/
 	
+	/**
+	 * Crea un nuevo usuario y lo anade al sistema
+	 * 
+	 * @param userName nombre del usuario (unico)
+	 * @param userNick nick del usuario (unico)
+	 * @param date fecha de nacimiento
+	 * @param userPass contrasena
+	 * 
+	 * @return ERROR si algo ha ido mal
+	 */
 	public Status register(String userName, String userNick, LocalDate date, String userPass) {
 		
 		if(userNameExists(userName) || userNickExists(userNick))
@@ -337,6 +533,14 @@ public class Sistem implements java.io.Serializable {
 		return Status.OK;
 	}
 	
+	/**
+	 * Loggea a un usuario al sistema
+	 * 
+	 * @param userName nick del usuario
+	 * @param userPass contrasena del usuario
+	 * 
+	 * @return ERROR si algo ha ido mal
+	 */
 	public Status login(String userName, String userPass) {
 		
 		if(loggedUser != null)
@@ -356,6 +560,11 @@ public class Sistem implements java.io.Serializable {
 		return Status.ERROR;
 	}
 	
+	/**
+	 * Desloggea al que estuviese loggeado en el sistema y guarda el sistema
+	 * 
+	 * @return ERROR en caso de IOException
+	 */
 	public Status logout() {
 		
 		loggedUser = null;
@@ -370,6 +579,11 @@ public class Sistem implements java.io.Serializable {
 		return Status.OK;
 	}
 	
+	/**
+	 * Checks if admin is logged
+	 * 
+	 * @return boolean
+	 */
 	public boolean adminIsLogged() {
 		
 		if (loggedUser == adminUser)
@@ -379,6 +593,16 @@ public class Sistem implements java.io.Serializable {
 	
 	/************************* Song Related ********************/
 	
+	/**
+	 * Busca a partir de la query con las restricciones oportunas
+	 * 
+	 * @param query string de busqueda
+	 * @param title busqueda por titulo
+	 * @param author busqueda por autor
+	 * @param album busqueda por album
+	 * 
+	 * @return lista con todos los resultados de busqueda
+	 */
 	public ArrayList<PlayableObject> search(String query, Boolean title, Boolean author, Boolean album) {
 		
 		ArrayList<PlayableObject> search = new ArrayList<PlayableObject>();
@@ -396,6 +620,13 @@ public class Sistem implements java.io.Serializable {
 		return search;
 	}
 	
+	/**
+	 * Busqueda por titulo
+	 * 
+	 * @param title titulo a buscar
+	 * 
+	 * @return lista con resultados
+	 */
 	private ArrayList<PlayableObject> searchTitle(String title) {
 		
 		ArrayList<PlayableObject> search = new ArrayList<PlayableObject>();
@@ -413,6 +644,13 @@ public class Sistem implements java.io.Serializable {
 		return search;
 	}
 	
+	/**
+	 * Busqueda por autor
+	 * 
+	 * @param author autor a buscar
+	 * 
+	 * @return lista con resultados
+	 */
 	private ArrayList<PlayableObject> searchAuthor(String author) {
 		
 		ArrayList<PlayableObject> search = new ArrayList<PlayableObject>();
@@ -430,6 +668,13 @@ public class Sistem implements java.io.Serializable {
 		return search;
 	}
 	
+	/**
+	 * Busqueda por album
+	 * 
+	 * @param albumTitle album a buscar
+	 * 
+	 * @return lista con resultados
+	 */
 	private ArrayList<PlayableObject> searchAlbum(String albumTitle) {
 		
 		ArrayList<PlayableObject> search = new ArrayList<PlayableObject>();
@@ -444,6 +689,14 @@ public class Sistem implements java.io.Serializable {
 	
 
 	/******************** Report related ******************/
+	
+	/**
+	 * Anade un report a la lista
+	 * 
+	 * @param r report a anadir
+	 * 
+	 * @return ERROR si r es null
+	 */
 	public Status addReport(Report r) {
 
 		if(r != null) {
@@ -454,6 +707,13 @@ public class Sistem implements java.io.Serializable {
 		return Status.ERROR;
 	}
 
+	/**
+	 * Elimina un report de la lista
+	 * 
+	 * @param r report a eliminar
+	 * 
+	 * @return ERROR si r es null
+	 */
 	public Status deleteReport(Report r) {
 
 		if(r != null) {
@@ -466,6 +726,11 @@ public class Sistem implements java.io.Serializable {
 
 	/******************* SAVE STATE ***********************/
 	
+	/**
+	 * Guarda los datos en un fichero System.bal
+	 * 
+	 * @throws IOException si hay problemas con el fichero
+	 */
 	public void saveData()throws IOException{
 	    String fileName= "System.bal";
 	    FileOutputStream fos = new FileOutputStream(fileName);
