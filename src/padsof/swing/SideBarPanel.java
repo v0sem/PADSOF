a9de0cfb5ee2dfdf4b6a506c42f188e9f80255aa
v0sem <progpabsanch@gmail.com>
2019-04-26 10:56:05 +0200

    SideBar done

--- src/padsof/swing/SideBarPanel.java
+++ src/padsof/swing/SideBarPanel.java
@@ -12,6 +12,7 @@ public class SideBarPanel extends JPanel {
 
 	private JPanel userInfo;
 	private JPanel menu;
+	private JPanel userActions;
 	
 	public SideBarPanel() {
 		SpringLayout layout = new SpringLayout();
@@ -20,15 +21,20 @@ public class SideBarPanel extends JPanel {
 		Sistem.getInstance().login("admin", "admin");
 		userInfo = new UserInfoLoggedPanel();
 		menu = new MenuPanel();
+		userActions = new UserActionsLoggedPanel();
 		
 		this.add(userInfo);
 		this.add(menu);
+		this.add(userActions);
 		
 		layout.putConstraint(SpringLayout.WEST, userInfo, 0, SpringLayout.WEST, this);
-		layout.putConstraint(SpringLayout.NORTH, userInfo, 10, SpringLayout.NORTH, this);
+		layout.putConstraint(SpringLayout.NORTH, userInfo, 5, SpringLayout.NORTH, this);
 		
 		layout.putConstraint(SpringLayout.WEST, menu, 0, SpringLayout.WEST, this);
-		layout.putConstraint(SpringLayout.NORTH, menu, 75, SpringLayout.NORTH, this);
+		layout.putConstraint(SpringLayout.NORTH, menu, 70, SpringLayout.NORTH, this);
+		
+		layout.putConstraint(SpringLayout.WEST, userActions, 0, SpringLayout.WEST, this);
+		layout.putConstraint(SpringLayout.NORTH, userActions, 315, SpringLayout.NORTH, this);
 		
 		this.setPreferredSize(new Dimension(200, 450));
 	}
