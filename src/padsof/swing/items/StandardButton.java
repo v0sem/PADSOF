f5b7bf6df3896a81c722d0591485af596693e85d
code406 <dpalomo99@gmail.com>
2019-05-07 10:59:36 +0200

    Fixed song adder. Hopefully fixes sidebar bug. Works here

--- src/padsof/swing/items/StandardButton.java
+++ src/padsof/swing/items/StandardButton.java
@@ -10,8 +10,8 @@ public class StandardButton extends JButton{
 
 	public StandardButton(String text, int x, int y) {
 		super(text);
-		Color c1 = new Color(0xff76cf); //button background color
-		Color c2 = new Color(0x212121); //text color
+		Color c1 = new Color(0xFF5722); //button background color. Pablo says it has to be 0xFF5722
+		Color c2 = new Color(0x212121); //text color. Pablo says it has to be 0xFFFFFF
 		this.setPreferredSize(new Dimension(x, y));
 		this.setFocusPainted(false);
 		this.setBackground(c1);
