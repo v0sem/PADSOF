93d35c75df2cbc25b56d6a40311d2367fff863a1
v0sem <progpabsanch@gmail.com>
2019-05-08 23:28:11 +0200

    COMMENT

--- src/padsof/swing/items/AlbumCreationPanel.java
+++ src/padsof/swing/items/AlbumCreationPanel.java
@@ -5,13 +5,28 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+/**
+ * Panel para crear un Nuevo Album
+ * 
+ * @author David Palomo, Pablo Sanchez, Antonio Solana
+ *
+ */
 @SuppressWarnings("serial")
 public class AlbumCreationPanel extends JPanel {
 	
+	/**
+	 * Titulo del nuevo Album
+	 */
 	JTextField titulo;
 	
+	/**
+	 * Ano del nuevo Album
+	 */
 	JTextField ano;
 
+	/**
+	 * Constructor de clase.
+	 */
 	public AlbumCreationPanel() {
 		titulo = new JTextField(5);
 		ano = new JTextField(5);
@@ -22,10 +37,20 @@ public class AlbumCreationPanel extends JPanel {
 		this.add(ano);	
 	}
 
+	/**
+	 * Getter del titulo
+	 * 
+	 * @return titulo del album
+	 */
 	public JTextField getTitulo() {
 		return titulo;
 	}
 
+	/**
+	 * Getter del ano
+	 * 
+	 * @return ano del album
+	 */
 	public JTextField getAno() {
 		return ano;
 	}
