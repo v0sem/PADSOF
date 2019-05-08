53bcadfefb2fd89848580dc8861890540bf4100b
code406 <dpalomo99@gmail.com>
2019-05-08 19:44:22 +0200

    Button to add songs to albums

--- src/padsof/control/MySongsControl.java
+++ src/padsof/control/MySongsControl.java
@@ -2,11 +2,16 @@ package padsof.control;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 
+import javax.swing.JOptionPane;
+
+import padsof.playable.Album;
 import padsof.playable.Song;
 import padsof.swing.MainFrame;
 import padsof.swing.MySongsPanel;
 import padsof.system.System;
+import padsof.user.User;
 
 public class MySongsControl implements ActionListener {
 
@@ -31,6 +36,32 @@ public class MySongsControl implements ActionListener {
 			if(panel.getPendingTable().getSelected().getClass() == Song.class) {
 				System.getInstance().deleteSong((Song) panel.getPendingTable().getSelected());
 				MainFrame.getInstance().mostrarMySongs();
+			}
+		}
+		if (event.getSource().equals(panel.getAddToAlbumButton())) {
+			Boolean found = false;
+			String title = JOptionPane.showInputDialog("¿A que album la quieres anadir?");
+			if (title != null) {
+				if (title.isEmpty()) {
+					JOptionPane.showMessageDialog(this.panel, "Es obligatorio rellenar el campo", "Error", JOptionPane.ERROR_MESSAGE);
+				}
+				else {
+					User author = System.getInstance().getLoggedUser();
+					List<Album> al = System.getInstance().getAlbumList();
+					for (Album a : al) {
+						if (a.getTitle().equals(title) && a.getAuthor().equals(author)) {
+							found = true;
+							// Meter objeto seleccionado si es una cancion
+							if(panel.getAcceptedTable().getSelected().getClass().equals(Song.class))
+								a.addSong((Song) panel.getAcceptedTable().getSelected());
+							break;
+						}
+					}
+				}
+				
+				if (!found) {
+					JOptionPane.showMessageDialog(this.panel, "Ese album no existe", "Error", JOptionPane.ERROR_MESSAGE);
+				}
 			}
 		}
 	}
